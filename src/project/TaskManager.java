4643e0578941b63df522c362956060ca1f19c287
sanzhar <sanzhar.agabaev@gmail.com>
2025-09-13 16:25:22 +0500

    Реализован метод markAsDone для отметки задачи выполненной

--- src/project/TaskManager.java
+++ src/project/TaskManager.java
@@ -6,14 +6,14 @@ public class TaskManager {
     private ArrayList<Task> tasks = new ArrayList<>();
     private int counter = 1;
 
-    //добавить задачу
+    //добавить задачу1
     public void addTask(String title) {
         Task task = new Task(counter++, title);
         tasks.add(task);
         System.out.println("Задача добавлена " + task);
     }
 
-        //показать все задачи
+        //показать все задачи2
         public void showTasks(){
             if(tasks.isEmpty()) {
                 System.out.println("Список задач пуст ");
@@ -24,8 +24,22 @@ public class TaskManager {
             }
         }
 
+        // отметить задачу выполненой 3
+    public void markAsDone(int id){
+        for(Task task : tasks){
+            if(task.getId() == id){
+                task.markAsDone();
+                System.out.println("Задача " +id + "отмечена выполненой");
+                return;
+            }
+        }
+        System.out.println("Задача с id " +id + "не найдена");
+    }
 
-
-
+    //удалить задачу4
+    public void removeTask(int id){
+        tasks.removeIf(task -> task.getId() == id);
+        System.out.println("Если задача с id " + id + "сущестовала - она удалена ");
+    }
 
 }
